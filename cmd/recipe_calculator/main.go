b1e5ffb6992ca2e5118618eb19a2df472a00a403
misarescu <misarescu@deviqon.com>
2024-03-27 15:18:46 +0200

    Add server and api handler template

--- cmd/recipe_calculator/main.go
+++ cmd/recipe_calculator/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"recipe-calculator.com/internal/server"
+)
+
+type response struct {
+	Message string
+}
 
 func main() {
-	fmt.Println("Hello calculator")
+	api, _ := server.New("localhost", "8080")
+
+	api.AddRouteHandlerFancy("GET /message", func(w http.ResponseWriter, r *http.Request) error {
+		w.Header().Set("Contenty-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		res := response{Message: "all is ok"}
+		err := json.NewEncoder(w).Encode(res)
+		return err
+	}, nil)
+
+	api.Run()
 }
---
+++ internal/server/main.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var shutdown os.Signal = syscall.SIGUSR1
+
+type Server struct {
+	listenAddr string
+	listenPort string
+	router     *http.ServeMux
+}
+
+type ApiHandler func(http.ResponseWriter, *http.Request) error
+type ErrorHandler func(error)
+
+func MakeApiHandler(f ApiHandler, e ErrorHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			if e != nil {
+				e(err) // handle the error here
+			}
+		}
+	}
+}
+
+func New(addr, port string) (*Server, error) {
+	return &Server{listenAddr: addr, listenPort: port, router: http.NewServeMux()}, nil
+}
+
+func (s *Server) AddRouteHandlerFancy(apiRoute string, f ApiHandler, e ErrorHandler) {
+	s.router.HandleFunc(apiRoute, MakeApiHandler(f, e))
+}
+
+func (s *Server) AddRouteHandler(apiRoute string, f http.HandlerFunc) {
+	s.router.HandleFunc(apiRoute, f)
+}
+
+func (s *Server) Run() {
+	addr := s.listenAddr + ":" + s.listenPort
+	server := &http.Server{Addr: addr}
+
+	http.Handle("/", s.router) // handle all routes
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	go func() {
+		log.Println("starting server on ", server.Addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalln(err.Error())
+			stop <- shutdown
+		}
+	}()
+
+	signal := <-stop
+	log.Println("Shutting down server ...")
+
+	server.Shutdown(context.TODO())
+
+	if signal == shutdown {
+		os.Exit(1)
+	}
+}
